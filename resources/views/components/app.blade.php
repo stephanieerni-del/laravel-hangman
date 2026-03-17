4aae21c2583b3d27c3a06567c119911a73e65460
ervinjavier <johnervin.javier@cvsu.edu.ph>
2026-03-17 23:17:11 +0800

    added favicon

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ config('app.name') }} - {{ $title }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/title.png ') }}" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
